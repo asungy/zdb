667c893537bb461017c6579d505b648d3adb0e10
asungy <62207329+asungy@users.noreply.github.com>
2025-03-30 13:07:17 -0400

    Added zig clap

--- build.zig
+++ build.zig
@@ -8,11 +8,18 @@ fn addExecutable(b: *std.Build) void {
         .optimize = b.standardOptimizeOption(.{}),
     });
 
+    const clap = b.dependency("clap", .{});
+    exe.root_module.addImport("clap", clap.module("clap"));
+
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run zdb");
     run_step.dependOn(&run_exe.step);
+
+    if (b.args) |args| {
+        run_exe.addArgs(args);
+    }
 }
 
 pub fn build(b: *std.Build) void {

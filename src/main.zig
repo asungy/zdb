7376507b62ff938803bdb9ea2ac38bc6196d3cf5
asungy <62207329+asungy@users.noreply.github.com>
2025-04-03 01:18:11 -0400

    Daily push: working on ptrace stuff

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,17 @@
 const clap = @import("clap");
 const std = @import("std");
+const linux = std.os.linux;
+
+fn attach() void {}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const params = comptime clap.parseParamsComptime(
-        \\-h, --help Display this help and exit.
+        \\-h, --help       Display this help and exit.
+        \\-p, --pid  <u16> PID of process to attach to.
+        \\<str>
     );
 
     var res = try clap.parse(clap.Help, &params, clap.parsers.default, .{
@@ -18,5 +23,13 @@ pub fn main() !void {
         return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
     }
 
-    std.debug.print("hello world\n", .{});
+    if (res.args.pid) |pid| {
+        if (linux.ptrace(linux.PTRACE.ATTACH, pid, 0, 0, 0) < 0) {
+            return error.PtraceAttachError;
+        }
+    } else if (res.positionals[0]) |program_path| {
+        std.debug.print("{s}", .{program_path});
+    } else {
+        return error.NoArguments;
+    }
 }
